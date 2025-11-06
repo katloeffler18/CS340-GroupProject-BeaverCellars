1c31d8164bb5cff3d17e4e43c066125649e954e4
katloeffler18 <katloeffler18@gmail.com>
2025-11-05 17:12:49 -0700

    Modified CSS. Created Home page component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Wines from './components/Wines';
 import Members from './components/Members';
 import CreditCards from './components/CreditCards';
@@ -10,18 +11,21 @@ function App() {
 
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/wines">Wines</Link> | <Link to="/members">Members</Link> 
-        | <Link to="/creditcards">Credit Cards</Link> | <Link to="/orders">Orders</Link> | <Link to="/shipments">Shipments</Link>  
-      </nav>
+      <header>
+        <h1>Beaver Cellars</h1>
+        <nav class="topnav">
+          <Link to="/">Home</Link><Link to="/wines">Wines</Link><Link to="/members">Members</Link> 
+          <Link to="/creditcards">Credit Cards</Link><Link to="/orders">Orders</Link><Link to="/shipments">Shipments</Link>  
+        </nav>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/wines" element={<Wines />} />
         <Route path="/members" element={<Members />} />
         <Route path="/creditcards" element={<CreditCards />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/shipments" element={<Shipments />} />
       </Routes>
+      </header>
     </Router>
   );
 }
---
+++ FrontEnd/src/components/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "75vh",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "28",
+          color: "black"
+        }}
+      >
+        Beaver Cellars Member and Order Management System
+      </h1>
+
+      <h2
+        style={{
+          fontSize: "24px",
+          fontWeight: "normal",
+          marginBottom: "1rem",
+          color: "black"
+        }}
+      >
+        Created by Kat Loeffler and Fuhai Feng
+      </h2>
+
+      <p style={{ color: "black", fontSize: "20px", maxWidth: "600px" }}>
+        Welcome! This database backend system will allow Beaver Cellars to 
+        efficiently track members, manage inventory, process orders, 
+        and coordinate shipments. Use the navigation above to view, add, 
+        edit, or delete Wines, Members, Credit Cards, Orders, and Shipments.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
