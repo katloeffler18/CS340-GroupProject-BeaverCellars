b1d384075833a719a68e8c7f372bbbb4b44ab294
katloeffler18 <katloeffler18@gmail.com>
2025-11-11 18:06:28 -0700

    Updated DMQ SQL to reflect changes to UI

--- DataBase/DMQ.sql
+++ DataBase/DMQ.sql
@@ -14,25 +14,28 @@ SELECT wineID, wineName, wineVariety, wineYear, winePrice, grapeRegion FROM Wine
 SELECT memberID, memberName, email, city, state, phoneNumber FROM Members;
 
 -- Select CreditCards table
-SELECT CreditCards.cardID, Members.memberName, CreditCards.cardName, CreditCards.cardNumber, CreditCards.cardExpirationDate, CreditCards.billingZipCode
+SELECT CreditCards.cardID, CreditCards.memberID, Members.memberName, CreditCards.cardName, CreditCards.cardNumber, CreditCards.cardExpirationDate, CreditCards.billingZipCode
 FROM CreditCards
 JOIN Members ON CreditCards.memberID = Members.memberID;
 
 -- Select Orders table
-SELECT Orders.orderID, Members.memberName, CreditCards.cardName, Orders.orderDate, Orders.orderPrice, Orders.hasShipped
+SELECT Orders.orderID, Orders.memberID, Members.memberName, Orders.cardID, CreditCards.cardName, Orders.orderDate, Orders.orderPrice, Orders.hasShipped
 FROM Orders
 JOIN Members ON Orders.memberID = Members.memberID
 JOIN CreditCards ON Orders.cardID = CreditCards.cardID;
 
 -- Select WinesOrders table
-SELECT WinesOrders.winesOrdersID, WinesOrders.orderID, Wines.wineName, WinesOrders.wineQuantity, WinesOrders.price
+SELECT WinesOrders.winesOrdersID, WinesOrders.orderID, Members.memberName, WinesOrders.wineID, Wines.wineName, WinesOrders.wineQuantity, WinesOrders.price
 FROM WinesOrders
-JOIN Wines ON WinesOrders.wineID = Wines.wineID;
+JOIN Wines ON WinesOrders.wineID = Wines.wineID
+JOIN Orders ON WinesOrders.orderID = Orders.orderID
+JOIN Members ON Orders.memberID = Members.memberID;
 
 -- Select Shipments table
-SELECT Shipments.shipmentID, Orders.orderID, Shipments.shipmentDate, Shipments.carrier, Shipments.trackingNumber
+SELECT Shipments.shipmentID, Shipments.orderID, Members.memberName, Shipments.shipmentDate, Shipments.carrier, Shipments.trackingNumber
 FROM Shipments
-JOIN Orders ON Shipments.orderID = Orders.orderID;
+JOIN Orders ON Shipments.orderID = Orders.orderID
+JOIN Members ON Orders.memberID = Members.memberID;
 
 -- ===================== INSERT =====================
 
@@ -44,53 +47,80 @@ VALUES (@wineNameInput, @wineVarietyInput, @wineYearInput, @winePriceInput, @gra
 INSERT INTO Members (memberName, email, address, city, state, memberZipCode, phoneNumber)
 VALUES (@memberNameInput, @emailInput, @addressInput, @cityInput, @stateInput, @zipInput, @phoneInput);
 
+-- Insert new credit card
+INSERT INTO CreditCards (memberID, cardName, cardNumber, cardExpirationDate, billingZipCode)
+VALUES (@memberIDinput, @cardNameInput, @cardNumberInput, @cardExpirationDateInput, @billingZipCodeInput);
+
 -- Insert new Order
 INSERT INTO Orders (memberID, cardID, orderDate, orderPrice, hasShipped)
-VALUES (
-    (SELECT memberID FROM Members WHERE memberName = @memberNameInput),
-    (SELECT cardID FROM CreditCards WHERE cardName = @cardNameInput),
-    @orderDateInput, @orderPriceInput, @hasShippedInput
-);
+VALUES (@memberIDInput, @cardIDInput, @orderDateInput, @orderPriceInput, @hasShippedInput);
 
 -- Insert new shipment
 INSERT INTO Shipments (orderID, shipmentDate, carrier, trackingNumber)
-VALUES (
-    (SELECT orderID FROM Orders WHERE orderNumber = @orderNumberInput),
-    @shipmentDateInput, @carrierInput, @trackingInput
-);
+VALUES (@orderIDInput, @shipmentDateInput, @carrierInput, @trackingInput);
 
--- Insert new Order
-INSERT INTO WineOrders (orderID, memberID, wineID, wineQuantity, price)
-VALUES (
-    (SELECT orderID FROM Orders WHERE orderID = @corderIDInput),
-    (SELECT memberID FROM Members WHERE memberName = @memberNameInput),
-    (SELECT wineID FROM Wines WHERE wineName = @wineNameInput),
-    @wineQuantityInput, @priceInput
-);
+-- Insert new wine into an order
+INSERT INTO WineOrders (orderID, wineID, wineQuantity, price)
+VALUES (@corderIDInput, @wineIDInput, @wineQuantityInput, @priceInput);
 
 -- ===================== UPDATE =====================
 
--- Update Wine price
-UPDATE Wines SET winePrice = @newPriceInput WHERE wineName = @wineNameInput;
-
--- Update Order shipping status
-UPDATE Orders SET hasShipped = TRUE WHERE orderID = @orderIDInput;
+-- Update Wine
+UPDATE Wines 
+SET wineName = @newWineNameInput, 
+    wineVariety = @newWineVarietyInput, 
+    wineYear = @newWineYearInput, 
+    winePrice = @newPriceInput,
+    grapeRegion = @newGrapeRegionInput
+WHERE wineID = @wineIDInput;
 
 -- Update Member address, city, state, and zip
 UPDATE Members
-SET address = @newAddressInput, city = @newCityInput, state = @newStateInput, memberZipCode = @newZipInput
-WHERE memberName = @memberNameInput;
+SET memberName = @newMemberNameInput,
+    email = @newEmailInput,
+    address = @newAddressInput, 
+    city = @newCityInput, 
+    state = @newStateInput, 
+    memberZipCode = @newZipInput
+    phoneNumber = @newPhoneNumberInput
+WHERE memberID = @memberIDInput;
+
+-- Update Credit Card info
+UPDATE CreditCards
+SET cardName = @newcardNameInput,
+    cardNumber = @newcardNumberInput,
+    cardExpirationDate = @newcardExpirationDateInput, 
+    billingZipCode = @newbillingZipCodeInput, 
+WHERE cardID = @cardIDInput;
+
+-- Update Order shipping status
+UPDATE Orders 
+SET hasShipped = @newHasShippedInput ,
+WHERE orderID = @orderIDInput;
+
+-- Update wine in an order
+UPDATE WinesOrders
+SET wineID = @newWineIDInput,
+    wineQuantity = @newWineQuantityInput,
+    price = @newPriceInput
+WHERE winesOrdersID = @winesOrdersIDInput
 
 -- ===================== DELETE =====================
+
+-- Delete wine
+DELETE FROM Wines WHERE wineID = @wineIDInput;
+
+-- Delete member
+DELETE FROM Members WHERE memberID = @memberIDInput;
+
+-- Delete credit card
+DELETE FROM CreditCards WHERE creditCardID = @creditcardIDInput;
 
 -- Delete order
 DELETE FROM Orders WHERE orderID = @orderIDInput;
 
--- Delete member
-DELETE FROM Members WHERE memberName = @memberNameInput;
-
--- Delete wine
-DELETE FROM Wines WHERE wineName = @wineNameInput;
-
--- Delete wine order
+-- Delete WinesOrders
 DELETE FROM WinesOrders WHERE wineOrderID = @wineOrderIDInput;
+
+-- Delete shipment
+DELETE FROM Shipments WHERE shipmentID = @shipmentIDInput;
